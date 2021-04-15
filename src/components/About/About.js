9ad29ecc49e3a08cf8de3a2991230d2bdef0e8e0
Taylor Weeks <taylorlweeks707@gmail.com>
2021-04-15 13:16:32 -0400

    added contact links and logo

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,10 @@
+import React from "react";
+
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className="about-container">
+            <h1>About Me</h1>
+            <p className="about-me">I am currently a full time student enrolled in the Full-Stack Flex Coding Bootcamp at the University of North Carolina Charlotte.</p>
         </div>
     );
 }
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import { FaGithub, FaLinkedinIn, FaEnvelope, FaFileAlt  } from "react-icons/fa";
+
 const Contact = () => {
     return (
-        <div>
+        <div className="contact-container">
             <h1>Contact</h1>
+            <div className="row">
+        <div className="links-me col text-center">
+          <a href="https://github.com/tweeks07" target="_blank" rel="noreferrer"><FaGithub size={44}/></a>
+          <a href="https://www.linkedin.com/in/taylor-weeks-2a43538a/" target="_blank" rel="noreferrer"><FaLinkedinIn size={44}/></a>
+          <a href="mailto: taylorlweeks707@gmail.com" target="_blank" rel="noreferrer"><FaEnvelope size={44}/></a>
+          <a href="https://documentcloud.adobe.com/link/review?uri=urn:aaid:scds:US:fc8e7cc6-99db-4de2-9cd6-9a8e29bf77d3" target="_blank" rel="noreferrer"><FaFileAlt size={40}/></a>
+          </div>
+          </div>
         </div>
     );
 }
