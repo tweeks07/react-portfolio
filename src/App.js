f4fb416609305a638003d6f25b1f01f23d6bddb0
Taylor Weeks <taylorlweeks707@gmail.com>
2021-04-14 20:14:17 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Work from './components/Work/Work';
+import Contact from './components/Contact/Contact';
+import { BrowserRouter , Route } from 'react-router-dom';
+
 
 function App() {
-  return (
+  return ( 
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      < NavBar />
+      <Route exact path = "/" component={ Home } />
+      <Route exact path = "/about" component={ About } />
+      <Route exact path = "/work" component={ Work } />
+      <Route exact path = "/contact" component={ Contact } />
+     
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link , useLink, useLocation } from "react-router-dom";
+
+
+const NavBar = () => {
+    const location = useLocation();
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className={`nav-item ${location.pathname === "/" && "active"}`}>
+                            <Link className="nav-link" to="/">Home </Link>
+                        </li>
+                        <li className={`nav-item ${location.pathname === "/about" && "active"}`}>
+                            <Link className="nav-link" to="/about">About</Link>
+                        </li>
+                        <li className={`nav-item ${location.pathname === "/work" && "active"}`}>
+                            <Link className="nav-link" to="/work">Work</Link>
+                        </li>
+                        <li className={`nav-item ${location.pathname === "/contact" && "active"}`}>
+                            <Link className="nav-link" to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
