91ff5e35f32324e4dffb2fc82f27d18a4c03553b
Taylor Weeks <taylorlweeks707@gmail.com>
2021-04-15 18:35:16 -0400

    finishing up

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,9 +1,89 @@
-const Work = () => {
+import React from "react";
+
+
+
+
+function Work() {
     return (
-        <div>
+        <div className="work-container">
             <h1>Work</h1>
+            <h3>EyeRead</h3>
+                <div className="row">
+                <div className="links-work col text-center">
+                <a href="https://eyeread-app.herokuapp.com/">
+                    <img src="/images/eyeread.png" className="eyeread" />
+                </a>
+
+                </div>
+
+            </div>
+
+            <h3>Restaurants Near Me</h3>
+            <div className="row">
+                <div className="links-work col text-center">
+                <a href="https://scashmore.github.io/Restaurants-Near-Me/">
+                    <img src="/images/restaurants.png" className="restaurants" />
+                </a>
+
+                </div>
+
+            </div>
+
+            <h3>Workday Planner</h3>
+            <div className="row">
+                <div className="links-work col text-center">
+                <a href="https://tweeks07.github.io/Day-Planner/">
+                    <img src="/images/dayplanner.png" className="dayplanner" />
+                </a>
+
+                </div>
+
+            </div>
+
+            <h3>Code Refactor</h3>
+            <div className="row">
+                <div className="links-work col text-center">
+                <a href="https://tweeks07.github.io/Code-Refactor/">
+                    <img src="/images/refactor.png" className="refactor" />
+                </a>
+
+                </div>
+
+            </div>
+
+            <h3>Fitness Tracker</h3>
+            <div className="row">
+                <div className="links-work col text-center">
+                <a href="https://ancient-falls-64004.herokuapp.com/">
+                    <img src="/images/fitness.png" className="fitness" />
+                </a>
+
+                </div>
+
+            </div>
+
+            <h3>Personal Portfolio</h3>
+            <div className="row">
+                <div className="links-work col text-center">
+                <a href="https://tweeks07.github.io/Personal-Portfolio/">
+                    <img src="/images/portfolio.png" className="portfolio" />
+                </a>
+
+                </div>
+
+            </div>
+
+
+
         </div>
-    );
+
+        
+
+
+
+
+
+    )
 }
 
 export default Work;
